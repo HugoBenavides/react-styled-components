34df29feb7268f9956024f80b24c43516bf88078
Hugo Benavides <hugo-dumask@gmail.com>
2023-08-23 22:45:02 -0600

    agregando un nuevo componente List con su estructuracion de datos de un objeto y su array

--- src/Components/List/List.jsx
+++ src/Components/List/List.jsx
@@ -1,11 +1,20 @@
+import React from "react";
 import { Box } from "../UI"
-import { lista } from "../../info";
+import { lista } from "../../info"; 
 
 
 const List = () => {
+    console.log(lista) // aprender a regresar un objeto con arreglo y destructurado usan un map para recorrerlo
     return <Box>
         {
-            <h2>Hola</h2>
+            lista.cargos.map( ({id, type, value, from, date})=>{
+                return <div key={id}>
+                    <span>{type}</span>
+                    <span>{value}</span>
+                    <span>{from}</span>
+                    <span>{date}</span>
+                </div>
+            } )
         }
     </Box>
 
