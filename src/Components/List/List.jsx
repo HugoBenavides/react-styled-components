f13d390d096d1dfea8a46b6bfd4df5053a3d8166
Hugo Benavides <the.another.gerf@gmail.com>
2023-08-24 19:10:50 -0600

    se comitea con los archivos del Card agregado junto con sus styles

--- src/Components/List/List.jsx
+++ src/Components/List/List.jsx
@@ -1,21 +1,18 @@
 import React from "react";
-import { Box } from "../UI"
+import { Boton, Box, boton } from "../UI"
 import { lista } from "../../info"; 
+import Card from "../Card/Card";
 
 
 const List = () => {
-    console.log(lista) // aprender a regresar un objeto con arreglo y destructurado usan un map para recorrerlo
+    // aprender a regresar un objeto con arreglo y destructurado usan un map para recorrerlo
     return <Box>
         {
-            lista.cargos.map( ({id, type, value, from, date})=>{
-                return <div key={id}>
-                    <span>{type}</span>
-                    <span>{value}</span>
-                    <span>{from}</span>
-                    <span>{date}</span>
-                </div>
+            lista.cargos.map( (cargo, i)=>{
+                return <Card key={i} cargo={cargo} />
             } )
         }
+    <Boton>Ver más</Boton>
     </Box>
 
 }
